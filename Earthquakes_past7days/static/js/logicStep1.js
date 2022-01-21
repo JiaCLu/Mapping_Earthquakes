a5ebe211ffb9f62e6830b1d33c816fdeb5bb282a
Jia Lu <jiajaja@jias-mbp.lan>
2022-01-20 18:01:20 -0800

    Add Earthquakes_past7days.

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,7 +1,7 @@
 console.log("working");
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   accessToken: 'pk.eyJ1IjoiYm9ib2NvbWZ5IiwiYSI6ImNreWtjcDFmeDJ2Nmsyb3FrbHA5NXY1MHAifQ.5j2-YpSh-LSOumlOGXtCAg'
@@ -17,14 +17,14 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Street": streets,
-  "Satellite Streets": satelliteStreets
+  "Satellite": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -35,11 +35,10 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the torontoData GeoJSON URL
 //let torontoData = "https://raw.githubusercontent.com/bobocomfy/Mapping_Earthquakes/main/torontoRoutes.json"
 // Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/bobocomfy/Mapping_Earthquakes/main/torontoNeighborhoods.json"
+//let torontoHoods = "https://raw.githubusercontent.com/bobocomfy/Mapping_Earthquakes/main/torontoNeighborhoods.json"
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data).addTo(map);
 });
